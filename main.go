b2b37804fe586dd79808bdf7ffe59c941d7465a8
Adrián10-07 <adrian2aguilarg@gmail.com>
2025-04-10 04:15:28 -0600

    Cors

--- main.go
+++ main.go
@@ -3,10 +3,19 @@ package main
 import (
 	"FLIGHTS_API/src/flights/infraestructure/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:    []string{"*"},
+		AllowCredentials: true,
+	}))
+
 	routes.SetupRoutes(router)
 
 	router.Run(":8080") 
